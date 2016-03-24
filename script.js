6574d676c2d79f4303e7bf5ea86fbbc373fb69fc
Smith Bhojak <99sbhojak@gmail.com>
2016-03-24 10:52:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 		confirm("Lets Play rock Paper Scissors. Click Ok to confirm");
 		var un = prompt("Enter your Name:");
+		var i ;
+		for(i=0;i<3;i++){
 		var cc = Math.random();
 		var uc  = prompt("Enter your choice in 'r,p,s' (r=rock,p=paper,s=scissors): ");
 		if (cc >= 0.1 && cc <0.4){
@@ -46,10 +48,12 @@
 			if(uc === "p") {
 			confirm( "Computer Wins");
 							}
-									}
-			else{
+								}
+					else{
 				confirm('try again');
-			}
+						}
+						
+			 }
 			var rating = prompt("Rate this Game Out 1 to 10");
 			if(rating >= 6){
 				confirm("Thank You " + un + " !");
@@ -57,5 +61,5 @@
 			else{
 				confirm("I will improve mys skils " + un +  " !");
 			}
+
 			confirm("Refresh Page If you find any 'Error' ");
-			
