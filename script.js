60010482b7afa19ab45337a967a1dbfa82f50aa7
Smith Bhojak <99sbhojak@gmail.com>
2016-03-24 08:07:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
-confirm("This is Two Player Game. Click Ok to confirm");
-		confirm("Lets Play rock Paper Scissors.");
-		var u1 = prompt("Enter Player 1's Name");
-		var u2 = prompt("Enter Player 2's Name");
-		confirm(" Now " +u1 + "'s Turn");
-		var u1c = prompt("Enter " + u1 +"'s Choice in Rock or Paper or Scissors By  r,p,s ");
-		confirm("And Now "+ u2 +"'s Turn")
-		var u2c = prompt("Enter " + u2 +"'s  Choice in Rock or Paper or Scissors By  r,p,s ");
-		if (u1c === "r" && u2c === "r") {
+		confirm("Lets Play rock Paper Scissors. Click Ok to confirm");
+		var un = prompt("Enter your Name:");
+		var cc = Math.random();
+		var uc  = prompt("Enter your choice: ");
+		if (cc >= 0.1 && cc <0.4){
+			if (uc === "r") {
 			confirm("Its a Tie try again!");
-		}
-		else if (u1c === "r" && u2c === "p") {
-			confirm( "" + u2 +"Wins");
-		}
-		else if (u1c === "r" && u2c === "s") {
-			confirm("So "+ u1 +" Wins");
-		}
-		else if (u1c === "p" && u2c === "r") {
-			confirm("So " + u1+" Wins");
-		}
-		else if (u1c === "s" && u2c === "r") {
-			confirm("And "+u2+" Wins");
-		}
-		else if (u1c === "p" && u2c === "p") {
-			confirm("Its a Tie try again");
-		}
-		else if (u1c === "s" && u2c ==="s"){
-			confirm("Its a tie try again");
-		} 
-		else if (u1c === "p" && u2c === "s"){
-			confirm("And "+u2+" Wins");
-		}
-		else if (u1c === "s" && u2c === "p"){	
-			confirm("And "+ u1+" Wins");
-		}
-		else{
-				confirm("Error occured try reloading The page & please Enter in either r or p or s");
-		}
-		confirm("If you find error try reloading The page");
+							}
+								}
+		else if(cc >= 0.4 && cc<0.7) {
+			if(uc === "r") {
+			confirm("Congrats " +un+" you Win");
+							}
+									}
+		else if (cc >= 0.7 && cc<1){
+			if(uc === "r") {
+			confirm( "Computer Wins");
+							}
+									}
+		if (cc >= 0.1 && cc <0.4){
+			if (uc === "s") {
+			confirm("Its a Tie try again!");
+							}
+								}
+		else if(cc >= 0.4 && cc<0.7) {
+			if(uc === "s") {
+			confirm("Congrats " +un+" you Win");
+							}
+									}
+		else if (cc >= 0.7 && cc<1){
+			if(uc === "s") {
+			confirm( "Computer Wins");
+							}
+									}
+		if (cc >= 0.1 && cc <0.4){
+			if (uc === "p") {
+			confirm("Its a Tie try again!");
+							}
+								}
+		else if(cc >= 0.4 && cc<0.7) {
+			if(uc === "p") {
+			confirm("Congrats " +un+" you Win");
+							}
+									}
+		else if (cc >= 0.7 && cc<1){
+			if(uc === "p") {
+			confirm( "Computer Wins");
+							}
+									}
+			else{
+				confirm('try again');
+			}
+			var rating = prompt("Rate this Game Out 1 to 10");
+			if(rating >= 6){
+				confirm("Thank You " + un + " !");
+			}
+			else{
+				confirm("I will improve mys skils " + un +  " !");
+			}
+			confirm("Refresh Page If you find any 'Error' ");
+			
