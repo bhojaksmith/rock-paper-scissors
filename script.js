c0d143d835d3cda0819071e00fd542fa8892cf4b
Smith Bhojak <99sbhojak@gmail.com>
2016-03-27 10:32:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
+		function script() {
 		confirm("Lets Play rock Paper Scissors. Click Ok to confirm");
 		var un = prompt("Enter your Name:");
-		var i ;
 		for(i=0;i<3;i++){
 		var cc = Math.random();
 		var uc  = prompt("Enter your choice in 'r,p,s' (r=rock,p=paper,s=scissors): ");
 		if (cc >= 0.1 && cc <0.4){
 			if (uc === "r") {
-			confirm("Its a Tie try again!");
+			document.getElementById("test").innerHTML="Its a Tie try again!";
 							}
 								}
 		else if(cc >= 0.4 && cc<0.7) {
 			if(uc === "r") {
-			confirm("Congrats " +un+" you Win");
+			document.getElementById("test").innerHTML="Congrats " +un+" you Win";
 							}
 									}
 		else if (cc >= 0.7 && cc<1){
 			if(uc === "r") {
-			confirm( "Computer Wins");
+			document.getElementById("test").innerHTML="Computer Wins";
 							}
 									}
 		if (cc >= 0.1 && cc <0.4){
 			if (uc === "s") {
-			confirm("Its a Tie try again!");
+			document.getElementById("test").innerHTML="Its a Tie try again!";
 							}
 								}
 		else if(cc >= 0.4 && cc<0.7) {
 			if(uc === "s") {
-			confirm("Congrats " +un+" you Win");
+				document.getElementById("test").innerHTML="Congrats " +un+" you Win";
 							}
 									}
 		else if (cc >= 0.7 && cc<1){
 			if(uc === "s") {
-			confirm( "Computer Wins");
+			document.getElementById("test").innerHTML= "Computer Wins";
 							}
 									}
 		if (cc >= 0.1 && cc <0.4){
 			if (uc === "p") {
-			confirm("Its a Tie try again!");
+				document.getElementById("test").innerHTML="Its a Tie try again!";
 							}
 								}
 		else if(cc >= 0.4 && cc<0.7) {
 			if(uc === "p") {
-			confirm("Congrats " +un+" you Win");
+			document.getElementById("test").innerHTML="Congrats " +un+" you Win";
 							}
 									}
 		else if (cc >= 0.7 && cc<1){
 			if(uc === "p") {
-			confirm( "Computer Wins");
+			document.getElementById("test").innerHTML="Computer Wins";
 							}
-								}
-					else{
-				confirm('try again');
-						}
-						
-			 }
-			var rating = prompt("Rate this Game Out 1 to 10");
-			if(rating >= 6){
-				confirm("Thank You " + un + " !");
-			}
+									}
 			else{
-				confirm("I will improve mys skils " + un +  " !");
+					document.getElementById("test").innerHTML='Congrats '+un+ " You Win!";
 			}
-
-			confirm("Refresh Page If you find any 'Error' ");
+				}
+					}
